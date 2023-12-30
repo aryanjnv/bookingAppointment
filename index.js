d39c83218c19d8785d83c1bfbadeb7c733562935
aryanjnv <aryangpjnv188@gmail.com>
2023-12-30 18:45:38 +0530

    delete on cloud implemented

--- index.js
+++ index.js
@@ -10,37 +10,74 @@ function handleFormSubmit(event) {
         email: email,
         phone: phone
     };
-    axios.post('https://crudcrud.com/api/e46d73537f1049cd86330acab07c52d1/appointment', userDetails)
+    axios.post('https://crudcrud.com/api/904259dd91b640a5b1a1a5a8b3a4d27f/appointment', userDetails)
         .then(function (response) {
             console.log('Data stored in the cloud:', response.data);
         })
         .catch(function (error) {
             console.error('Error storing data in the cloud:', error);
         });
+        initUI();
     document.getElementById('username').value = '';
     document.getElementById('email').value = '';
     document.getElementById('phone').value = '';
 }
 
 function initUI() {
-    axios.get('https://crudcrud.com/api/e46d73537f1049cd86330acab07c52d1/appointment')
+    axios.get('https://crudcrud.com/api/904259dd91b640a5b1a1a5a8b3a4d27f/appointment')
         .then(function (response) {
             var userDetailsArray = response.data;
 
             var userList = document.getElementById('userList');
-            userList.innerHTML = ''; 
+            userList.innerHTML = ''; // Clear previous entries
 
             userDetailsArray.forEach(function (userDetails) {
                 var listItem = document.createElement('li');
-                // Exclude the ID from the displayed information
+
                 listItem.textContent = `Username: ${userDetails.username}, Email: ${userDetails.email}, Phone: ${userDetails.phone}`;
+
+
+                var deleteButton = document.createElement('button');
+                deleteButton.innerHTML = 'Delete';
+                deleteButton.style.cursor = 'pointer';
+                deleteButton.onclick = function () {
+                    deleteUser(userDetails._id);
+                };
+
+        
+                listItem.appendChild(deleteButton);
+
+                
                 userList.appendChild(listItem);
             });
         })
         .catch(function (error) {
+            
             console.error('Error fetching data from the cloud:', error);
         });
 }
+
+function deleteUser(userId) {
+   
+    axios.delete(`https://crudcrud.com/api/904259dd91b640a5b1a1a5a8b3a4d27f/appointment/${userId}`)
+        .then(function (response) {
+            
+            console.log('User deleted from the cloud:', response.data);
+            var userList = document.getElementById('userList');
+            var userToDelete = userList.querySelector(`li[data-user-id="${userId}"]`);
+            
+            if (userToDelete) {
+                userList.removeChild(userToDelete);
+            }
+
+            initUI();
+        })
+        .catch(function (error) {
+            
+            console.error('Error deleting user from the cloud:', error);
+        });
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
     initUI();
